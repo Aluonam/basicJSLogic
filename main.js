e9ebefb8642f801b910d8b3e23d19f75c04b8e3d
Paula Cano <paulacanoroman@gmail.com>
2023-09-30 14:41:44 +0200

    Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7
    array con numeros entre los que dividir y de ahí hacemos .some(()=>{})

--- main.js
+++ main.js
@@ -37,3 +37,16 @@ const vocalesFrase = (frase) =>{
 }
 vocalesFrase("mi nombre es pau")
 
+// Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+
+const divisibleNum = (num)=>{
+    const numerosDivision = [2, 3, 5, 7]
+   const division = numerosDivision.some((element)=>num % element === 0)
+   console.log(division)
+          
+  }
+  
+  divisibleNum(287)
+  // false
+  
+
