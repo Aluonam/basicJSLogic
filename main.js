8efbf812eedefd361e65f7af767b985e7c1e7903
Paula Cano <paulacanoroman@gmail.com>
2023-09-30 14:38:37 +0200

    Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a
    .split() .filter()

--- main.js
+++ main.js
@@ -6,3 +6,15 @@ const divisible = (num)=>{
 }
   
  divisible(5)
+ //false
+
+
+
+// Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a
+const letraA = (frase)=>{
+  const letras= frase.split("").filter((letra)=>letra == "a")
+  console.log(letras.length)
+}
+
+letraA("hola que tal estas")
+
