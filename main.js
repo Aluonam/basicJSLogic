dcb40d0a922b09e5f20306fdeb69c73e10ee497b
Paula Cano <paulacanoroman@gmail.com>
2023-09-30 14:37:16 +0200

    Escribe un programa que pida un número y diga si es divisible por 2

---
+++ main.js
@@ -0,0 +1,8 @@
+// Escribe un programa que pida un número y diga si es divisible por 2
+
+const divisible = (num)=>{
+  const isDivisible = num%2 === 0
+  console.log(isDivisible)
+}
+  
+ divisible(5)
