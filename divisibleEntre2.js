f01a53ea934ebab7d051ee4041d625eaada5bdd0
Aluon <paulacanoroman@gmail.com>
2023-12-23 20:18:55 +0100

    feat: mostrar divisible entre 2

--- divisibleEntre2.js
+++ divisibleEntre2.js
@@ -1,10 +1,11 @@
 // Escribe un programa que pida un número y diga si es divisible por 2
 
+const num = prompt("Escribe un número ¿Será divisible entre dos?")
 const divisible = (num)=>{
     const isDivisible = num%2 === 0
-    document.write("El número es divisible: ",isDivisible)
+    document.write("<br>","El número es divisible: ",isDivisible)
   }
     
-   divisible(5)
+   divisible(num)
    //false
   
