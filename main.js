ab2b6dbc6ecfe3ed52cfdd7e8b9722a6800ebedf
Aluon <paulacanoroman@gmail.com>
2023-12-23 19:56:07 +0100

    feat: enlazar con html

--- main.js
+++ main.js
@@ -9,16 +9,6 @@ const divisible = (num)=>{
  //false
 
 
-
-// Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a
-const letraA = (frase)=>{
-  const letras= frase.split("").filter((letra)=>letra == "a")
-  console.log(letras.length)
-}
-
-letraA("hola que tal estas")
-
-
 // Escribe un programa que pida una frase y escriba las vocales que aparecen
 const vocalesFrase = (frase) =>{
   
