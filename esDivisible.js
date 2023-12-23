4a09274313011a0ee9797edb380e347d73b7ef4b
Aluon <paulacanoroman@gmail.com>
2023-12-23 20:25:55 +0100

    feat: mostrar en pantalla es divisible

--- esDivisible.js
+++ esDivisible.js
@@ -1,15 +1,15 @@
+// Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 
+//(sólo hay que comprobar si lo es por uno de los cuatro)
 
+const number = prompt("Escribe un número ¿Será divisible entre 2, 3, 5 o 7?") 
 
-// Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
-
-const divisibleNum = (num)=>{
+const divisibleNum = (number)=>{
     const numerosDivision = [2, 3, 5, 7]
-   const division = numerosDivision.some((element)=>num % element === 0)
-   console.log(division)
+    const division = numerosDivision.some((element)=>num % element === 0)
+    document.write("<br> Es divisible entre 2, 3, 5 o 7: ",division)
           
   }
   
-  divisibleNum(287)
-  // false
-  
+  divisibleNum(number)
+
 
