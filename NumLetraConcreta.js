37e565c2f1923c300bb65eb5488efa013f07016f
Aluon <paulacanoroman@gmail.com>
2023-12-23 20:28:53 +0100

    feat: mostrar por pantalla número de veces que aparece la letra A

--- NumLetraConcreta.js
+++ NumLetraConcreta.js
@@ -1,8 +1,10 @@
-
 // Escribe un programa que pida una frase y escriba cuantas veces aparece la letra a
-const letraA = (frase)=>{
-    const letras= frase.split("").filter((letra)=>letra == "a")
-    console.log(letras.length)
+
+const fraseUsuario = prompt("Escribe una frase para saber cuantas veces aparece la letra A: ")
+
+const letraA = (fraseUsuario)=>{
+    const letras= fraseUsuario.split("").filter((letra)=>letra == "a")
+    document.write("<br> El número de veces que aparece la letra A es: ",letras.length)
   }
   
-  letraA("hola que tal estas")
+  letraA(fraseUsuario)
