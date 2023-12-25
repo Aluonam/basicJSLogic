161fbadb4b3efa404cac3a2361908d81dc30d653
Aluon <paulacanoroman@gmail.com>
2023-12-25 20:51:41 +0100

    feat: if else compare

--- palindromo.js
+++ palindromo.js
@@ -4,7 +4,13 @@ const palabra = prompt("Escribe una palabra para comprobar si es un palíndromo"
 const palindromo = (palabra)=>{
     const palabraIntroducida = palabra.split("")
     const delReves = palabra.split("").reverse()
-
+    
+    if(palabraIntroducida.join("") === delReves.join("")){
+        document.write("Esta palabra SI es un palíndromo")
+    } 
+    else{
+        document.write("Esta palabra NO es un palíndromo")
+    }
 }
 
 palindromo(palabra)
