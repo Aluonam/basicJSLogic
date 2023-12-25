3ae03544a588ad18efbe6e0f80c261ee4623f1d2
Aluon <paulacanoroman@gmail.com>
2023-12-25 20:42:24 +0100

    feat: reverse word

--- palindromo.js
+++ palindromo.js
@@ -1,5 +1,9 @@
 
 const palabra = prompt("Escribe una palabra para comprobar si es un palíndromo")
-const palindromo = ()=>{
 
+const palindromo = ()=>{
+    const delReves = palabra.split("").reverse()
+    console.log(delReves)
 }
+
+palindromo()
