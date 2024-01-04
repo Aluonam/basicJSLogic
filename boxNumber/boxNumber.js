2ffb6d1a8307f6a66283b654fe24d6b908afaed6
Paula Cano <paulacanoroman@gmail.com>
2024-01-04 17:47:17 +0100

    feat: charge html div before script js

--- boxNumber/boxNumber.js
+++ boxNumber/boxNumber.js
@@ -5,7 +5,7 @@ var box = document.getElementById("box");
 for(let i = 0; i<5; i++){
     div = document.createElement("div");
     div.onclick = function () {
-        alert("This is box # " + i);
+        alert("This is box # " + (i + 1));
     };
     box.appendChild(div);
 } 
